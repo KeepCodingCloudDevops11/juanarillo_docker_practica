237aa7b0cd9fa08c295746bb6e5b400b22d2f7b9
Juan Arillo <juan.arillo@apppadel.com>
2025-01-16 17:08:49 +0100

    Added logs

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 COPY requirements.txt /app/requirements.txt
 
 # Install the dependencies
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the current directory contents into the container at /app
 COPY . /app
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import time
 import redis
 from flask import Flask
 import os
+import logging
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +13,27 @@ app = Flask("app")
 REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "name": record.name,
+            "time": self.formatTime(record),
+        }
+        if record.exc_info:
+            log_record["exception"] = self.formatException(record.exc_info)
+        return json.dumps(log_record)
+
+# Configuración del logger
+logger = logging.getLogger("jsonLogger")
+
+handler = logging.StreamHandler()
+handler.setFormatter(JsonFormatter())
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+# Configuración de la caché de Redis
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
@@ -28,4 +51,5 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
+    logger.info("Request recibido")
     return 'Redis host: {}, Redis port: {} . Hello World! I have been seen {} times.\n'.format(REDIS_HOST, REDIS_PORT, count)
